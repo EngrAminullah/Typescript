5391688ad17c34bd4bce309047764d5fce6d3d80
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-08-15 17:50:05 +0500

    arrays

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,28 @@
 // if (age < 50)
 //     age += 10
 
-let sales: number = 123_456_789;
-let course: string = 'typescript';
-let is_published = true;
-let level;
-level = 1;
-level = 'a';
+// let sales: number = 123_456_789;
+// let course: string = 'typescript';
+// let is_published = true;
+// let level;
+// level = 1;
+// level = 'a';
 
-function render(document) {
-    console.log(document)
-}
+// function render(document) {
+//     console.log(document)
+// }
+
+let names = ['daiza', 'maiza', 'faiza'];
+names.push('baiza')
+// names.push(3)
+// names[0]=3
+
+let numbers = [10, 20, 30, 40];
+numbers.push(25)
+// numbers.push('shaun');
+// numbers[1]='shaun'
+
+let mixed = ['ken', 4, 'chun-li', 8, 9, true]
+mixed.push('ryu')
+mixed.push(10)
+mixed[0] = 3
