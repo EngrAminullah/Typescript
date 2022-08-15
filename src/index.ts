78160be9be14eb54ad7b1bf7d71c1c9205071bc1
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-08-15 17:38:51 +0500

    The any type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
-// console.log('Hello its ma birthday')
-let age: number = 20;
-if (age < 50)
-    age += 10
+// // console.log('Hello its ma birthday')
+// let age: number = 20;
+// if (age < 50)
+//     age += 10
+
+let sales: number = 123_456_789;
+let course: string = 'typescript';
+let is_published = true;
+let level;
+level = 1;
+level = 'a';
+
+function render(document) {
+    console.log(document)
+}
