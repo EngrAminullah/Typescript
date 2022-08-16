51cc953f23874def68cb12e9b40640797bfca486
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-08-16 13:59:30 +0500

    Function basics

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,15 @@
 "use strict";
-console.log('test');
-let a = 123;
+let greet;
+greet = () => {
+    console.log('hello a,amir');
+};
+const add = (a, b, c = 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, '10');
+const minus = (a, b) => {
+    return a + b;
+};
+let result = minus(10, 7);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -112,9 +112,23 @@
 // ninja = { name: 'youshi', age: true };
 // console.log(ninja)
 
-console.log('test')
+// console.log('test')
 
-let a = 123
+// let a = 123
 
 
+let greet: Function;
+greet = () => {
+    console.log('hello a,amir')
+}
+const add = (a: number, b: number, c: number | string = 10) => {
+    console.log(a + b)
+    console.log(c)
+}
+add(5, 10, '10')
 
+const minus = (a: number, b: number) => {
+    return a + b
+}
+
+let result = minus(10, 7)
