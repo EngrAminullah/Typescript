e20a02aa30c4e8f1ae4ea56dc78ce4cd839b1d0f
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-08-16 11:42:17 +0500

    little environment

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,17 @@
 "use strict";
-let age = 20;
-if (age < 50)
-    age += 10;
+let character;
+let age;
+let is_published;
+age = 30;
+is_published = true;
+let ninjas = [];
+ninjas.push("shaun");
+let mixed = [];
+mixed.push("hello");
+mixed.push(12);
+mixed.push(false);
+let ninjaOne;
+ninjaOne = { name: "youshi", age: 30 };
+let nijaTwo;
+nijaTwo = { name: "mario", age: 20, beltcolor: "black" };
 //# sourceMappingURL=index.js.map
--- src/index.js
+++ src/index.js
@@ -2,36 +2,28 @@
 // let age: number = 20;
 // if (age < 50)
 //     age += 10
-
 // let sales: number = 123_456_789;
 // let course: string = 'typescript';
 // let is_published = true;
 // let level;
 // level = 1;
 // level = 'a';
-
 // function render(document) {
 //     console.log(document)
 // }
-
 // let names = ['daiza', 'maiza', 'faiza'];
 // names.push('baiza')
 // // names.push(3)
 // // names[0]=3
-
 // let numbers = [10, 20, 30, 40];
 // numbers.push(25)
 // // numbers.push('shaun');
 // // numbers[1]='shaun'
-
 // let mixed = ['ken', 4, 'chun-li', 8, 9, true]
 // mixed.push('ryu')
 // mixed.push(10)
 // mixed[0] = 3;
-
-
 // //---------------objects--------------
-
 // let ninja = {
 //     name: 'mario',
 //     belt: 'black',
@@ -40,39 +32,26 @@
 // console.log(ninja)
 // ninja.age = 40;
 // ninja.name = 'ryu';
-
-
 ///---------------explicit Types--------------------------
-
-let character: string;
-let age: number;
-let is_published: boolean;
-
+var character;
+var age;
+var is_published;
 // age='luigi';
 age = 30;
-
 // is_published=25;
 is_published = true;
-
 //-----arrays--------
-let ninjas: string[] = [];
-ninjas.push('shaun')
-
+var ninjas = [];
+ninjas.push('shaun');
 // ---------------Union types---------------
-let mixed: (string | number | boolean)[] = [];
+var mixed = [];
 mixed.push('hello');
 mixed.push(12);
-mixed.push(false)
-console.log(mixed)
-
+mixed.push(false);
+console.log(mixed);
 //-------------objects---------------------
-let ninjaOne: object;
+var ninjaOne;
 ninjaOne = { name: 'youshi', age: 30 };
-
-let nijaTwo: {
-    name: string,
-    age: number,
-    beltcolor: string
-}
-nijaTwo = { name: 'mario', age: 20, beltcolor: 'black' }
-console.log(nijaTwo)
+var nijaTwo;
+nijaTwo = { name: 'mario', age: 20, beltcolor: 'black' };
+console.log(nijaTwo);
