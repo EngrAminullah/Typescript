aba3f74d9b35c96e67f5596c4bd4fa06a777a87b
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-08-16 18:44:54 +0500

    raw material

--- src/index.ts
+++ src/index.ts
@@ -144,32 +144,34 @@
 //     console.log(`${user.name} says hello`)
 // }
 
-///////////------------- function signature--------------
-//-----exmpl 1
-let greet: (a: string, b: string) => void;
+// ///////////------------- function signature--------------
+// //-----exmpl 1
+// let greet: (a: string, b: string) => void;
 
-greet = (name: string, greeting: string) => {
-    console.log(`${name} says ${greeting}`)
-}
+// greet = (name: string, greeting: string) => {
+//     console.log(`${name} says ${greeting}`)
+// }
 
-//--------------exmp2
+// //--------------exmp2
 
-let calc: (a: number, b: number, c: string) => number;
+// let calc: (a: number, b: number, c: string) => number;
 
-calc = (numOne: number, numTwo: number, action: string) => {
-    if (action === 'add') {
-        return numOne + numTwo
-    } else {
-        return numOne - numTwo
-    }
-}
+// calc = (numOne: number, numTwo: number, action: string) => {
+//     if (action === 'add') {
+//         return numOne + numTwo
+//     } else {
+//         return numOne - numTwo
+//     }
+// }
 
-//----------exmpl 3--------
+// //----------exmpl 3--------
 
-let logDetails: (obj: { name: string, age: number }) => void;
-type person = { name: string, age: number }
+// let logDetails: (obj: { name: string, age: number }) => void;
+// type person = { name: string, age: number }
 
-logDetails = (ninja: person) => {
-    console.log(`${ninja.name} is ${ninja.age} years old`)
-}
+// logDetails = (ninja: person) => {
+//     console.log(`${ninja.name} is ${ninja.age} years old`)
+// }
+
+//---------- DOM Interaction & Typecasting-------
 
